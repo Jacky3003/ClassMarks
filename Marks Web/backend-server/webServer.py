6f0843086a34127cd0e2343d2852db5475541fbc
jacky chen <jackychen4@gmail.com>
2023-04-12 16:07:53 -0400

    Simple connection from Pandas to Flask Backend

--- Marks Web/backend-server/webServer.py
+++ Marks Web/backend-server/webServer.py
@@ -2,15 +2,20 @@ from flask import Flask
 import pandas as pd
 import openpyxl as op
 
+new_df = pd.read_excel("C:\\Users\\Jacky chen\\Documents\\GitHub\\ClassMarks\\Marks Spreadsheet\\Marks.xlsx", sheet_name=0)
+mark_num = new_df.loc[0, "Current Mark"]; print(mark_num)
+new_wb = pd.ExcelFile("C:\\Users\\Jacky chen\\Documents\\GitHub\\ClassMarks\\Marks Spreadsheet\\Marks.xlsx")
+name_of_class = new_wb.sheet_names[0]
+
 app = Flask(__name__)
 
 @app.route("/classMarks")
 def classMarks():
-    return {"classMarks": ["60"]}
+    return {"classMarks": [mark_num]}
 
 @app.route("/className")
 def className():
-    return {"className":["CSCB36"]}
+    return {"className":[name_of_class]}
 
 if __name__ == '__main__':
     app.run(debug=True)
